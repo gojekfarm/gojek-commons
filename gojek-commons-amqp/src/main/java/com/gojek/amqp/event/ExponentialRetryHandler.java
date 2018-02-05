1f36180e641a6b1b53603e71640f021376b2387b
Satvik <nsatvik@gmail.com>
2018-02-05 10:47:10 +0530

    Handling edge cases in exponential retry handler

--- gojek-commons-amqp/src/main/java/com/gojek/amqp/event/ExponentialRetryHandler.java
+++ gojek-commons-amqp/src/main/java/com/gojek/amqp/event/ExponentialRetryHandler.java
@@ -1,14 +1,10 @@
 package com.gojek.amqp.event;
 
 import com.gojek.amqp.AmqpConnection;
-import com.gojek.core.event.Consumer;
 import com.gojek.core.event.Destination;
-import com.gojek.util.serializer.Serializer;
-import com.rabbitmq.client.AMQP;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Map;
 
 /**
  * Created by satvik on 04/02/18.
@@ -16,6 +12,8 @@ import java.util.Map;
 public class ExponentialRetryHandler<E> extends FixedRetryHandler<E> {
 
     private static final Logger logger = LoggerFactory.getLogger(ExponentialRetryHandler.class);
+
+    private static final int EXPONENT = 2;
 
     /**
      * @param connection
@@ -32,8 +30,11 @@ public class ExponentialRetryHandler<E> extends FixedRetryHandler<E> {
      * @param maxRetries
      */
     protected void waitBetweenRetries(int failureCount, int maxRetries) {
+        if (failureCount == 0) {
+            return;
+        }
         try {
-            Thread.sleep((long) (Math.pow(2, failureCount) * 1000));
+            Thread.sleep((long) (Math.pow(EXPONENT, failureCount) * 1000));
         } catch (InterruptedException ie) {
             logger.warn("Interrrupted when waiting between retries.");
         }
